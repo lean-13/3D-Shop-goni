ba127298bcba8a07d6071d092b7a2f60df72460a
leandro goñi <leandrodavid13@hotmail.com>
2022-03-15 21:41:12 -0300

    import bootstrap borrado de elementos inutiles

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// scss
+import './scss/navBar.scss'
+// navBar
+import Navbar from './components/navegacion/navbar'
+// react
+import React from 'react';
+
+function App() {
+  return (
+    <header className="App-header">
+      <Navbar/>
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/navegacion/navbar.js
@@ -0,0 +1,39 @@
+
+import '../../scss/navBar.scss';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-lg nav__bar--modificacion nav__bar--size nav__bar--color" id="navBar">
+        <div className="navBarSection">
+          <a className="navbar-brand nav__bar__logo--modificacion nav__bar__logo--size nav__bar__logo--color" id="navBarLogo" href="index.html">Logo</a>
+        </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="navBarSectionItems">
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="index.html">Inicio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Categorias</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Encargo</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Carrito</a>
+              </li>
+            </ul>
+            <span className="navbar-text">
+              3DShop
+            </span>
+          </div>
+        </div>
+      </nav>
+      );
+}
+
+export default Navbar;
