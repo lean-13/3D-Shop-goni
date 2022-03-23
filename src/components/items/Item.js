0935fe0ecb5b48dcb7d51f153e5a5bde7818ce3e
leandro goñi <leandrodavid13@hotmail.com>
2022-03-23 00:47:45 -0300

    arreglo img

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -6,7 +6,7 @@ import React from 'react';
 export const Item = ({img, precio, stock, categoria, descripcion}) => {
     return (
         <div classname="card">
-            <img classname="card-img-top" src={img} alt="Card image"/>
+            <img classname="card-img-top" src={img} alt="Card image" />
             <div classname="card-body">
                 <p classname="card-text">Precio: ${precio}</p>
                 <p classname="card-text">Stock: {stock}</p>
--- src/components/items/ItemList.js
+++ src/components/items/ItemList.js
@@ -11,28 +11,28 @@ const ProductoData = [
         descripcion: "maceta de las Diosas del amor, la belleza y la fertilidad",
         precio: 4000,
         stock: 5,
-        img: "../img/productos/1.jpg"
+        img: require("../../img/productos/1.jpg")
       },
       {
         nombre: "Casa para pajaros caravana",
         descripcion: "casa pajaros con forma de caravana perfecta para tu hogar",
         precio: 1200,
         stock: 2,
-        img: "../img/productos/2.jpg"
+        img: require("../../img/productos/2.jpg")
       },
       {
         nombre: "Lampara articulada star wars",
         descripcion: "lampara del famoso caza tie fighter",
         precio: 3000,
         stock: 4,
-        img: "../img/productos/3.jpg"
+        img: require("../../img/productos/3.jpg")
       },
       {
         nombre: "soporte de esponja con forma de bob esponja",
         descripcion: "ideal para darle un aire fresco a tu cocina",
         precio: 800,
         stock: 4,
-        img: "../img/productos/4.jpg"
+        img: require("../../img/productos/4.jpg")
       }
 ]
 
@@ -41,7 +41,7 @@ export const ItemList = () => {
         <div>
             {
                 ProductoData.map((item) => (
-                    <Item nombre={Item.nombre} precio={Item.precio} stock={Item.stock} categoria={Item.categoria} descripcion={Item.descripcion}/>
+                    <Item nombre={item.nombre} precio={item.precio} img={item.img} stock={item.stock} categoria={item.categoria} descripcion={item.descripcion}/>
                 ))
             }
         </div>
