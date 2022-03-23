b96a763e7696d2a25966e4560d5cf80c85e0ad65
leandro goñi <leandrodavid13@hotmail.com>
2022-03-23 16:51:27 -0300

    listaproductos

--- src/components/items/ItemList.js
+++ src/components/items/ItemList.js
@@ -1,5 +1,5 @@
 // react
-import React, { useEffect } from 'react';
+import React from 'react';
 // item
 import { Item } from './Item'
 // sass
@@ -7,61 +7,12 @@ import '../../scss/items/itemList.scss'
 
 const Data = '../../data/dataProducto.json'
 
-const ProductoData = [
-    {
-        nombre: "maceta de venus y afrodita",
-        descripcion: "maceta de las Diosas del amor, la belleza y la fertilidad",
-        precio: 4000,
-        stock: 5,
-        img: require("../../img/productos/1.jpg"),
-        alt: "img maceta diosas del amor"
-      },
-      {
-        nombre: "Casa para pajaros caravana",
-        descripcion: "casa pajaros con forma de caravana perfecta para tu hogar",
-        precio: 1200,
-        stock: 2,
-        img: require("../../img/productos/2.jpg"),
-        alt: "img casa de pajaros"
-      },
-      {
-        nombre: "Lampara articulada star wars",
-        descripcion: "lampara del famoso caza tie fighter",
-        precio: 3000,
-        stock: 4,
-        img: require("../../img/productos/3.jpg"),
-        alt: "img lampara star wars"
-      },
-      {
-        nombre: "soporte de esponja con forma de bob esponja",
-        descripcion: "ideal para darle un aire fresco a tu cocina",
-        precio: 800,
-        stock: 4,
-        img: require("../../img/productos/4.jpg"),
-        alt: "img soporte de bob esponja"
-      }
-]
-
-export const ItemList = () => {
-  const GetData = new Promise((resolve, reject) => {
-    let condicion = true;
-    if (condicion) {
-      setTimeout(() => {
-        resolve(ProductoData)
-      })
-    }else {
-      reject('error')
-    }
-  })
-  useEffect(() => {
-    GetData
-    .then((res) => console.log(res))
-  }, [])
-  
+export const ItemList = (ListaProductos) => {
+ 
   return (
       <div className="cardCatalogo">
           {
-              ProductoData.map((item) => (
+              ListaProductos.map((item) => (
                   <Item nombre={item.nombre} precio={item.precio} img={item.img} stock={item.stock} categoria={item.categoria} descripcion={item.descripcion} alt={item.alt}/>
               ))
           }
--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -1,5 +1,5 @@
 // react
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // sass
 import '../../scss/header.scss'
 // item count
@@ -7,12 +7,65 @@ import ItemCount from './ItemCount'
 import {ItemList} from './ItemList';
 const initial = 1;
 
+const ProductoData = [
+    {
+        nombre: "maceta de venus y afrodita",
+        descripcion: "maceta de las Diosas del amor, la belleza y la fertilidad",
+        precio: 4000,
+        stock: 5,
+        img: require("../../img/productos/1.jpg"),
+        alt: "img maceta diosas del amor"
+      },
+      {
+        nombre: "Casa para pajaros caravana",
+        descripcion: "casa pajaros con forma de caravana perfecta para tu hogar",
+        precio: 1200,
+        stock: 2,
+        img: require("../../img/productos/2.jpg"),
+        alt: "img casa de pajaros"
+      },
+      {
+        nombre: "Lampara articulada star wars",
+        descripcion: "lampara del famoso caza tie fighter",
+        precio: 3000,
+        stock: 4,
+        img: require("../../img/productos/3.jpg"),
+        alt: "img lampara star wars"
+      },
+      {
+        nombre: "soporte de esponja con forma de bob esponja",
+        descripcion: "ideal para darle un aire fresco a tu cocina",
+        precio: 800,
+        stock: 4,
+        img: require("../../img/productos/4.jpg"),
+        alt: "img soporte de bob esponja"
+      }
+]
+
 const OnAdd = (contador) => {
     alert('stock: ' + contador)
     console.log(contador)
 }
 
 const ItemListContainer = ({nombre, categoria, precio, stock}) => {
+
+    const [ListaProductos , SetListaProductos] = useState([])
+    const GetData = new Promise((resolve, reject) => {
+      let condicion = true;
+      if (condicion) {
+        setTimeout(() => {
+          resolve(ProductoData)
+        })
+      }else {
+        reject('error')
+      }
+    })
+    useEffect(() => {
+      GetData
+      .then((res) => SetListaProductos(res))
+      .catch((error) => console.log(error))
+    }, [])
+
     return (
         <div>
             <div className="itemProductoInfo">
@@ -30,7 +83,7 @@ const ItemListContainer = ({nombre, categoria, precio, stock}) => {
                     />
             </div>
             <div>
-                <ItemList/>
+                <ItemList ListaProductos={ListaProductos}/>
             </div>
         </div>
 
