47f0739c45828a9a48809135ef44d5a5d3bbc263
leandro goñi <leandrodavid13@hotmail.com>
2022-03-27 16:40:31 -0300

    fake api

--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -5,6 +5,9 @@ import '../../scss/header.scss'
 // item count
 import ItemCount from './ItemCount'
 import ItemList from './ItemList';
+// item
+import { GetData } from './FakeApi'
+
 const initial = 1;
 
 
@@ -15,63 +18,14 @@ const OnAdd = (contador) => {
 
 const ItemListContainer = ({nombre, categoria, precio, stock}) => {
 
-  const Productos = [
-    {
-        id: 1,
-        nombre: "maceta de venus y afrodita",
-        descripcion: "maceta de las Diosas del amor, la belleza y la fertilidad",
-        precio: 4000,
-        stock: 5,
-        img: require("../../img/productos/1.jpg"),
-        alt: "img maceta diosas del amor"
-      },
-      {
-        id: 2,
-        nombre: "Casa para pajaros caravana",
-        descripcion: "casa pajaros con forma de caravana perfecta para tu hogar",
-        precio: 1200,
-        stock: 2,
-        img: require("../../img/productos/2.jpg"),
-        alt: "img casa de pajaros"
-      },
-      {
-        id: 3,
-        nombre: "Lampara articulada star wars",
-        descripcion: "lampara del famoso caza tie fighter",
-        precio: 3000,
-        stock: 4,
-        img: require("../../img/productos/3.jpg"),
-        alt: "img lampara star wars"
-      },
-      {
-        id: 4,
-        nombre: "soporte de esponja con forma de bob esponja",
-        descripcion: "ideal para darle un aire fresco a tu cocina",
-        precio: 800,
-        stock: 4,
-        img: require("../../img/productos/4.jpg"),
-        alt: "img soporte de bob esponja"
-      }
-    ]
-
     const [ListaProductos , SetListaProductos] = useState([]);
     const [Cargando, SetCargando] = useState([])
-    const GetData = new Promise((resolve, reject) => {
-      let condicion = true;
-      if (condicion) {
-        setTimeout(() => {
-          resolve(Productos)
-        }, 2000)
-      }else {
-        reject('error')
-      }
-    })
     useEffect(() => {
       SetCargando(true);
       GetData
       .then((res) => SetListaProductos(res))
       .catch((error) => console.log(error))
-      .finally(() => SetCargando(false))
+      .finally(() => SetCargando(false  ))
     }, [])
     return (
         <div>
