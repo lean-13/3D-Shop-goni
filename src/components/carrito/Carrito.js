faf591d0e514404f0590a66c264fbac2d3d24db1
unknown <leandrodavid13@hotmail.com>
2022-04-03 20:03:13 -0300

    useContext en carrito

--- src/components/carrito/Carrito.js
+++ src/components/carrito/Carrito.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // scss
 import '../../scss/carrito/Carrito.scss'
 // iconos
 // carrito
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import { CartContext } from '../../context/CartContext';
 
 function Carrito() {
+
+    const { cart } = useContext(CartContext)
+    console.log(cart)
+
     return (
         <div className='divCarrito'>
             <h1><ShoppingCartOutlinedIcon
