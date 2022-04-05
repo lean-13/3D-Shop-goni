d90f27614e6bd6d66ad7b9cc980f598ddb2d7005
unknown <leandrodavid13@hotmail.com>
2022-04-05 15:46:12 -0300

    botones compra

--- src/components/carrito/Carrito.js
+++ src/components/carrito/Carrito.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 // scss
 import '../../scss/carrito/Carrito.scss'
+// boton
+import BotonVolver from '../buttons/ButtonVolver'
 // iconos
 // carrito
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
@@ -16,6 +18,29 @@ function Carrito() {
             <h1><ShoppingCartOutlinedIcon
             className='iconoCarrito'
             />Carrito</h1>
+            <div className='carritoProductos'>
+                {
+                    cart.map((item) => (
+                        <div className='itemProductoCarrito'>
+                            <div className='imgCarrito'>
+                                <img  src={item.img} alt={item.alt} />
+                            </div>
+                            <div className='productoCarritoInformacion'>
+                                <p>{item.nombre}</p>
+                                <p>Precio: ${item.precio}</p>
+                                <p>Cantidad: {item.cantidad}</p>
+                            </div>
+                            <div className='CarritoBotonDelete'>
+                                <BotonVolver/>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='botonesBorradoCompra'>
+                <button className='botonReset'>Borrar Carrito</button>
+                <button className='botonShop'>Comprar Carrito</button>
+            </div>
         </div>
 
     );
