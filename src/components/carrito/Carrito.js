14ede603c61de78b63d9b89b2d787366b8c05954
unknown <leandrodavid13@hotmail.com>
2022-04-07 14:23:05 -0300

    modificacion de return en carrito

--- src/components/carrito/Carrito.js
+++ src/components/carrito/Carrito.js
@@ -1,65 +1,69 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 // scss
-import '../../scss/carrito/Carrito.scss'
+import "../../scss/carrito/Carrito.scss";
 // iconos
 // carrito
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import { CartContext } from '../../context/CartContext';
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import { CartContext } from "../../context/CartContext";
 // remove
-import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 
 function Carrito() {
+  const { cart, CartTotal, VaciarCarrito, RemoveItem } =
+    useContext(CartContext);
 
-    const { cart, CartTotal, VaciarCarrito, RemoveItem } = useContext(CartContext)
-
+  if (cart.length === 0) {
     return (
-        <div className='divCarrito'>
-            <h1><ShoppingCartOutlinedIcon
-                className='iconoCarrito'
-                />Carrito</h1>
-               
-            {   
-                cart.length === 0 ?
-                <div className='divCarritoVacio'>
-                    <p className='mensaje subrayadoText'>Su carrito se encuentra vacio</p>
-                    <Link to='/' className='botonVolverInicio'>Volver a inicio</Link>
-                </div>
-                :
-                <>
-                <div className='carritoProductos'>
-                    {
-                        cart.map((item) => (
-                            <div className='itemProductoCarrito' key={item.id }>
-                                <div className='imgCarrito'>
-                                    <img  src={item.img} alt={item.alt} />
-                                </div>
-                                <div className='productoCarritoInformacion'>
-                                    <p>{item.nombre}</p>
-                                    <p>Precio: ${item.precio * item.cantidad}</p>
-                                    <p>Cantidad: {item.cantidad}</p>
-                                </div>
-                                <div className='CarritoBotonDelete'>
-                                    <button onClick={() => RemoveItem(item.id)} className='botonCartRemove'><RemoveShoppingCartIcon className='iconoCartRemove'/></button>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className='seccionControlCarrito'>
-                    <div>
-                        <h3>Precio Total: ${CartTotal()}</h3>
-                    </div>
-                    <div className='botonesBorradoCompra'>
-                        <button onClick={VaciarCarrito} className='botonReset'>Borrar Carrito</button>
-                        <button className='botonShop'>Comprar Carrito</button>
-                    </div>
-                </div>
-                </>
-            }
-        </div>
-
+      <div className="divCarrito divCarritoVacio">
+        <p className="mensaje subrayadoText">Su carrito se encuentra vacio</p>
+        <Link to="/" className="botonVolverInicio">
+          Volver a inicio
+        </Link>
+      </div>
     );
+  }
+  return (
+    <div className="divCarrito">
+      <h1>
+        <ShoppingCartOutlinedIcon className="iconoCarrito" />
+        Carrito
+      </h1>
+      <div className="carritoProductos">
+        {cart.map((item) => (
+          <div className="itemProductoCarrito" key={item.id}>
+            <div className="imgCarrito">
+              <img src={item.img} alt={item.alt} />
+            </div>
+            <div className="productoCarritoInformacion">
+              <p>{item.nombre}</p>
+              <p>Precio: ${item.precio * item.cantidad}</p>
+              <p>Cantidad: {item.cantidad}</p>
+            </div>
+            <div className="CarritoBotonDelete">
+              <button
+                onClick={() => RemoveItem(item.id)}
+                className="botonCartRemove"
+              >
+                <RemoveShoppingCartIcon className="iconoCartRemove" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="seccionControlCarrito">
+        <div>
+          <h3>Precio Total: ${CartTotal()}</h3>
+        </div>
+        <div className="botonesBorradoCompra">
+          <button onClick={VaciarCarrito} className="botonReset">
+            Borrar Carrito
+          </button>
+          <button className="botonShop">Comprar Carrito</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Carrito;
