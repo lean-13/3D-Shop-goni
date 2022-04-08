f5d4a27604340afcdf44db85bf5e776931b609fb
unknown <leandrodavid13@hotmail.com>
2022-04-07 22:01:30 -0300

    card text disponible segun stock

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -6,7 +6,7 @@ import { Card, Button } from 'react-bootstrap'
 import '../../scss/items/item.scss'
 
 const Item = ({producto}) => {
-    const {nombre, categoria, img, alt, precio, id} = producto;
+    const {nombre, categoria, img, alt, precio, id, stock} = producto;
     return (
         <Card className="cardProducto" style={{ width: '15rem' }}>
             <Card.Img src={img} alt={alt} />
@@ -14,6 +14,8 @@ const Item = ({producto}) => {
                 <Card.Title className='textScss'>{nombre}</Card.Title>
                 <Card.Text>Precio: ${precio}</Card.Text>
                 <Card.Text>Categoria: {categoria}</Card.Text>
+                {stock === 0 && <Card.Text style={{color:'orange'}}>Disponible solo para encargo</Card.Text> }
+                {stock >= 1 && <Card.Text style={{color:'green'}}>Disponible</Card.Text> }
                 <Link to={`/detail/${id}`}><Button className="botonVerMas">Ver mas</Button></Link>
             </Card.Body>
         </Card>
