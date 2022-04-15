e6f3b694bbd3bb479265b1838a0a19d5122ebf1c
unknown <leandrodavid13@hotmail.com>
2022-04-15 19:48:41 -0300

    checkout

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,9 +1,24 @@
-
-
+// react
+import {useContext} from 'react'
+import {CartContext} from '../../context/CartContext'
 // scss
 import './Checkout.scss'
 
 const Checkout = () => {
+
+    const {cart, cartTotal} = useContext(CartContext);
+
+    const Orden = {
+        items: cart,
+        total: cartTotal(),
+        comprador: {
+            nombre: 'lean',
+            email: 'leandro@hotmail.com',
+            celular: '234213'
+        }
+    }
+
+    console.log(Orden)
 
     return (
         <div className='checkout'>
