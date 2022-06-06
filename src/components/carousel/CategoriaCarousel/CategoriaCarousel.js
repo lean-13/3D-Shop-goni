a9fce92ad47d955bee918225a805881a567760ec
leandro goñi <leandrodavid13@hotmail.com>
2022-06-06 16:15:37 -0300

    img

--- src/components/carousel/CategoriaCarousel/CategoriaCarousel.js
+++ src/components/carousel/CategoriaCarousel/CategoriaCarousel.js
@@ -11,7 +11,7 @@ const CategoriaCarousel = () => {
                 <Carousel.Item className="inicioCarouselItem item1">
                     <img
                     className="d-block"
-                    src={("../../../../img/carousel/3d-printer-3311587.png")}
+                    src={("../../../../img/carousel/3d-printer-3311587.jpg")}
                     alt="First slide"
                     />
                     <Carousel.Caption className="carouselDescripcion">
@@ -21,7 +21,7 @@ const CategoriaCarousel = () => {
                 <Carousel.Item className="inicioCarouselItem item2">
                     <img
                     className="d-block"
-                    src={("../../../../img/carousel/3d-printer-3311587.png")}
+                    src={("../../../../img/carousel/3d-printer-3311587.jpg")}
                     alt="Second slide"
                     />
                     <Carousel.Caption className="carouselDescripcion">
@@ -31,7 +31,7 @@ const CategoriaCarousel = () => {
                 <Carousel.Item className="inicioCarouselItem item3">
                     <img
                     className="d-block"
-                    src={("../../../../img/carousel/3d-printer-3311587.png")}
+                    src={("../../../../img/carousel/3d-printer-3311587.jpg")}
                     alt="Third slide"
                     />
 
