a763b403a2648619504ab38d8feaff4346651408
leandro goñi <leandrodavid13@hotmail.com>
2022-10-04 11:51:35 -0300

    img

--- src/components/carousel/CategoriaCarousel/CategoriaCarousel.js
+++ src/components/carousel/CategoriaCarousel/CategoriaCarousel.js
@@ -7,7 +7,6 @@ const CategoriaCarousel = () => {
     return (
         <div className="divCategoriaCarousel">
             <Carousel className="categoriaCarousel">
-                {/* agregar imagen de menor tamaño y ponerle un div con fondo de color */}
                 <Carousel.Item className="categoriaCarouselItem itemcategoria1">
                     <img
                     className="d-block"
@@ -18,7 +17,7 @@ const CategoriaCarousel = () => {
                 <Carousel.Item className="categoriaCarouselItem itemcategoria2">
                     <img
                     className="d-block"
-                    src={("https://firebasestorage.googleapis.com/v0/b/dshop-ca6b4.appspot.com/o/img%2Fcarrousel%2Forganizador3D.jpg?alt=media&token=477c4e8a-abcb-4300-9b22-031c67d57890")}
+                    src={("https://firebasestorage.googleapis.com/v0/b/dshop-ca6b4.appspot.com/o/img%2Fcarrousel%2Fhogar3D.jpg?alt=media&token=7327fab3-36da-43d9-9e80-e9e98a2bfabf")}
                     alt="Second slide"
                     />
                     <Carousel.Caption className="carouselCategoriaDescripcion">
